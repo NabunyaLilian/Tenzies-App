1081d24a1d3a00f9ce0c2e364e183d6ba1df6934
Nabunya Lilian <liliannabunya2@gmail.com>
2022-07-20 10:03:52 +0300

    implement game algorithm

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import React from "react"
 import Die from "./Die"
-
+import { nanoid } from 'nanoid'
+import Confetti from "react-confetti"
 
 
 
 export default function App(){
 
    const [dice, setDice] = React.useState(getNewDice())
+   const [tenzies, setTenzies] = React.useState(false)
 
+
+   React.useEffect(
+       ()=> {
+           const allHeld = dice.every(die=> die.isHeld)
+           const firstValue = dice[0].value
+           const sameValues = dice.every(die=> die.value === firstValue)
+
+           if(allHeld && sameValues){
+             setTenzies(true)
+             console.log("You have won")
+           }
+           
+        },
+       [dice])
+
+
+   function generateNewDie(){
+       return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+   }
    
    function getNewDice(){
         const arr = []
         for(let i=0;  i < 10; i++){
             arr.push(
-                {
-                    value: Math.ceil(Math.random() * 6),
-                    isHeld: false,
-                    id: i
-                }
+                generateNewDie()
             )
         }
         return arr ;
    }
 
    function rollDice(){
-       setDice(getNewDice());
+       if(tenzies){
+            setDice(getNewDice())
+            setTenzies(false)
+       }else{
+            setDice(oldDice=> oldDice.map(die=>{
+                return die.isHeld? die: generateNewDie()
+            }))
+       };
    }
 
    function holdDice(id){
@@ -34,14 +62,25 @@ export default function App(){
    }
 
     const diceElements = dice.map(die=>{
-        return (<Die value={die.value} isHeld={die.isHeld} holdDice={()=>holdDice(die.id)}/>)
+        return (
+            <Die 
+                key={die.id}
+                value={die.value}
+                isHeld={die.isHeld}
+                holdDice={()=>holdDice(die.id)}
+                />)
     })
 
     return (
-    <main>
-        <div className="dice-board">
-          {diceElements}
-        </div>
-        <button onClick={rollDice}>Roll</button>
-    </main>)
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-board">
+            {diceElements}
+            </div>
+            <button onClick={rollDice}>
+                {tenzies? "New Game": "Roll"}
+            </button>
+        </main>)
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from 'react-dom/client';
 import App from "./App"
 import "./style.css"
 
 
 
 
-ReactDOM.render(<App />, document.getElementById("root")) ;
+createRoot(document.getElementById("root")).render(<App />) ;
